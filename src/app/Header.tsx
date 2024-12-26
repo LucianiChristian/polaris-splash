829db014d43dff50243d37efdcd37fbfade6464f
christianluciani <luciani_christian@yahoo.com>
2024-12-26 14:54:24 -0600

    Create basic header.

---
+++ src/app/Header.tsx
@@ -0,0 +1,18 @@
+export default function Header() {
+    return (
+        <div className="flex justify-between items-center px-8 py-2">
+            <div className="flex items-center gap-10">
+                <div className="flex items-center gap-4">
+                    <img src="/polaris-logo.png" alt="polaris logo" width={60}/>
+                    <div className="text-xl font-semibold">Polaris North Star</div>
+                </div>
+                <nav className="flex gap-8">
+                    <a href={"#features"} className="font-semibold text-slate-600">Features</a>
+                    <a href={"#FAQ"} className="font-semibold text-slate-600">FAQ</a>
+                </nav>
+            </div>
+            <button>Sign up</button>
+        </div>
+    );
+}
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import Header from "@/app/Header";
+
 export default function Home() {
   return (
-    <div>hi</div>
+    <Header />
   );
 }
